315197b2a9c165f4f06f443af03434fe4ef7dfa6
Matias <matisilva18@hotmail.com>
2015-10-29 22:24:02 -0300

    Fix push anterior funcion de validarUpload

--- Code/fotolink/fotolink/settings.py
+++ Code/fotolink/fotolink/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'fotolink.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'User/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
