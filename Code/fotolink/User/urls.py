de0bb83f515f42954dfb1d183617400cda8cc2e7
Guillermo Incatasciato <incatasciato.g.l@gmail.com>
2015-10-31 20:49:39 -0300

    quite el form de register

--- Code/fotolink/User/urls.py
+++ Code/fotolink/User/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
-from User.views import Register2
+from User.views import Register
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(r'^register/', Register2.as_view(), name='registro'),
+    url(r'^register/', Register.as_view(), name='registro'),
     url(r'^reg_ok/', TemplateView.as_view(template_name='User/reg_ok.html')),
 ]
--- Code/fotolink/User/views.py
+++ Code/fotolink/User/views.py
@@ -1,14 +1,11 @@
-from forms import UserForm
+from forms import UserCreationForm
 from django.views.generic import CreateView
 from django.contrib.auth import views
 
 
-class Register2(CreateView):
+class Register(CreateView):
     template_name = 'User/register.html'
-    form_class = UserForm
+    form_class = UserCreationForm
     success_url = '/reg_ok/'
 
-def change_password(request):
-    template_response = views.password_change(request)
-    # Do something with `template_response`
-    return template_response
+
