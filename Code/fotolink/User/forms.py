0c19ed494bd2e46e7566c4d48b41e13d310a3862
David <davidsansfamaf@gmail.com>
2015-10-31 14:15:09 -0300

    Borrado de comentarios, agregado de confirmacion de contraseña y encriptamiento al registro de usuario. Listo para revisión

--- Code/fotolink/User/forms.py
+++ Code/fotolink/User/forms.py
@@ -1,16 +1,8 @@
 from django.contrib.auth.models import User
-from django import forms
-
-'''
-class UserForm(forms.Form):
-    name = forms.CharField(max_length=40)
-    password = forms.CharField(max_length=32, widget=forms.PasswordInput)
-
-'''
+from django.contrib.auth.forms import UserCreationForm
 
 
-class UserForm(forms.ModelForm):
+class UserForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'password']
-        widgets = {'password': forms.PasswordInput(), }
+        fields = ['username', 'password1']
--- Code/fotolink/User/views.py
+++ Code/fotolink/User/views.py
@@ -2,25 +2,6 @@ from forms import UserForm
 from django.views.generic import CreateView
 
 
-'''
-def register(request):
-    print "oime"
-    if request.method == "POST":
-        print "aca toy"
-        form = UserForm(request.POST)
-        if form.is_valid():
-            username= form.cleaned_data['name']
-            password= form.cleaned_data['password']
-            new_user = User.objects.create_user(username=username, password=password)
-            new_user.save()
-            # redirect, or however you want to get to the main view
-            return HttpResponseRedirect('reg_ok.html')
-    else:
-        form = UserForm()
-        return render(request, 'User/register.html', {'form': form})
-'''
-
-
 class Register2(CreateView):
     template_name = 'User/register.html'
     form_class = UserForm
